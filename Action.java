941cb412f61d43c9757c50fc60b8f9e13b052d7b
Jason <redwrddd@gmail.com>
2021-05-23 12:59:24 -0600

    second commit

---
+++ Action.java
@@ -0,0 +1,20 @@
+public class Action {
+
+	private int value;
+	private int x;
+	private int y;
+
+	public Action(int number, int xCoor, int yCoor) {
+
+		value = number;
+		x = xCoor;
+		y = yCoor;
+	}
+
+	public int getValue() { return value; }
+	public int getX() { return x; }
+	public int getY() { return y; }
+	public void setValue(int v) { value = v; }
+	public void setX(int xValue) { x = xValue; }
+	public void setY(int yValue) { y = yValue; }
+}
---
+++ Application.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Application {
+
+	private int[][] inputGraph;
+	private int[][] outputGraph;
+	
+	public Application() {
+		
+		inputGraph = initializeInput();
+
+		outputGraph = solveInput();
+	}
+
+	public int[][] solveInput() {
+		
+		ArrayList<Action> actions = new ArrayList<Action>(1);
+		boolean solved = false;
+		boolean unSolvable = false;
+
+		while(!solved && !unSolvable) {
+			
+			// TODO		
+		}
+	}
+
+	private int[][] initializeInput() {
+		
+		int[][] tempGraph = new int[9][9] {
+			{0, 0, 3, 0, 0, 0, 1, 0, 8},
+			{8, 0, 0, 0, 9, 3, 6, 0, 0},
+			{9, 0, 2, 1, 0, 0, 3, 0, 4},
+			{2, 0, 0, 0, 1, 0, 0, 7, 0},
+			{0, 0, 4, 0, 2, 0, 9, 0, 0},
+			{0, 9, 0, 0, 6, 0, 0, 0, 5},
+			{4, 0, 9, 0, 0, 1, 5, 0, 2},
+			{0, 0, 8, 2, 5, 0, 0, 0, 9},
+			{7, 0, 5, 0, 0, 0, 4, 0, 0}
+		}
+
+		return tempGraph;
+	}
+}
